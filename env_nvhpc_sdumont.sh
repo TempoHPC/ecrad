5d8b3a2d5fa8ab277464be6e51a0125fde850666
rpsouto <rpsouto@lncc.br>
2024-04-04 16:25:12 -0300

    Changes to be committed: 	new file:   env_nvhpc_sdumont.sh

---
+++ env_nvhpc_sdumont.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+module load sequana/current
+module load git/2.23_sequana
+module load cmake/3.23.2_sequana
+module load make/make-4.3_sequana
+module load python/3.8.2_sequana
+module load gcc/9.3_sequana
+alias python='python3.8'
+alias python3='python3.8'
+module use /scratch/cenapadrjsd/rpsouto/opt/nvidia/hpc_sdk/modulefiles
+module load nvhpc/22.2
+. /scratch/cenapadrjsd/rpsouto/sequana/usr/local/spack/git/spack_nvhpc_22.2/share/spack/setup-env.sh
+export SPACK_USER_CONFIG_PATH=/scratch/cenapadrjsd/rpsouto/.spack/v0.17.1_nvhpc_22.2
+
