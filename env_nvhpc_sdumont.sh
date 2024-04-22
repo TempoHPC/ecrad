11878968dd19feaf1a3ec22a5155549555949330
rpsouto <rpsouto@lncc.br>
2024-04-21 22:48:23 -0300

    Changes to be committed: 	modified:   env_nvhpc_sdumont.sh

--- env_nvhpc_sdumont.sh
+++ env_nvhpc_sdumont.sh
@@ -3,15 +3,18 @@
 module load sequana/current
 module load git/2.23_sequana
 module load cmake/3.23.2_sequana
-module load make/make-4.3_sequana
-module load python/3.8.2_sequana
-module load gcc/9.3_sequana
-alias python='python3.8'
-alias python3='python3.8'
+module load python/3.9.1_sequana
+module load gcc/8.3_sequana
 module use /scratch/cenapadrjsd/rpsouto/opt/nvidia/hpc_sdk/modulefiles
 module load nvhpc/22.2
-. /scratch/cenapadrjsd/rpsouto/sequana/usr/local/spack/git/spack_nvhpc_22.2/share/spack/setup-env.sh
-export SPACK_USER_CONFIG_PATH=/scratch/cenapadrjsd/rpsouto/.spack/v0.17.1_nvhpc_22.2
 
-#Carrega o netcdf-fortran@4.5.3
-spack load /uvcnbiv
+workdir=/scratch/cenapadrjsd/rpsouto
+version=v0.17.1
+partition=sequana
+spackdir=${workdir}/spack/${partition}/${version}
+. ${spackdir}/share/spack/setup-env.sh
+
+export SPACK_USER_CONFIG_PATH=${workdir}/.spack/${partition}/${version}
+export SPACK_USER_CACHE_PATH=/tmp
+
+spack load netcdf-fortran@4.5.3%nvhpc@22.2
